3042cc612f7c710b02a22cfb44dc969e08ddf2e4
Peter Hirsch <peter.hirsch@mdc-berlin.de>
2021-01-08 11:29:32 -0500

    add efficientnet implementation

--- funlib/learn/tensorflow/models/__init__.py
+++ funlib/learn/tensorflow/models/__init__.py
@@ -1,4 +1,5 @@
-from .conv4d import conv4d # noqa
+from .conv4d import conv4d# noqa
+from .DepthwiseConv3D import depthwise_conv_3d# noqa
 from .unet import unet # noqa
 from .vgg import vgg # noqa
 from .layers import (conv, conv_pass,
@@ -6,4 +7,5 @@ from .layers import (conv, conv_pass,
                      repeat,
                      upsample)
 from .utils import crop
-from .resnet import resnet # noqa
+from .resnet import resnet  # noqa
+from .efficientnet import EfficientNetBX # noqa
